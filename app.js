ab5fd313b5ab991a672fca4ce937a31cbc63065e
won <bonav00@naver.com>
2022-05-01 22:33:25 +0900

    lego

---
+++ app.js
@@ -0,0 +1,33 @@
+// @ts-check
+
+/**
+ *
+ * @param {*} a
+ * @param {*} b
+ * @returns
+ */
+
+/**
+ *
+ * @param {number} a 첫번째 숫자
+ * @param {number} b 두번째 숫자
+ * @returns number
+ */
+function sum(a, b) {
+  return a + b;
+}
+
+sum(1, "2");
+
+/**
+ * @typedef {object} Address
+ * @property {string} street
+ * @property {string} city
+ */
+
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {Address} address
+ */
